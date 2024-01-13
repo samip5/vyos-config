b5d3d91677c2db48236f82190b96db4700091919
Skyler Mäntysaari <sm+git@skym.fi>
2024-01-13 17:44:06 +0200

    Update configs in Git

--- config-parts/service-router_advert.sh
+++ config-parts/service-router_advert.sh
@@ -1,10 +1,14 @@
 #!/bin/vbash
 
-set interface eth8.10 default-preference 'high'
-set interface eth8.10 interval max '600'
-set interface eth8.10 prefix ::/64 valid-lifetime '2592000'
+set service router-advert interface eth0.10 default-preference 'high'
+set service router-advert interface eth0.10 interval max '600'
+set service router-advert interface eth0.10 name-server '2001:67c:1104:9::4'
+set service router-advert interface eth0.10 prefix ::/64 valid-lifetime '2592000'
 
-set interface eth8.105 default-preference 'high'
-set interface eth8.105 interval max '600'
-set interface eth8.105 prefix ::/64 valid-lifetime '2592000'
+set service router-advert interface eth0.105 name-server '2001:67c:1104:9::4'
+set service router-advert interface eth0.105 prefix ::/0 valid-lifetime '2592000'
 
+set service router-advert interface eth0.110 default-preference 'high'
+set service router-advert interface eth0.110 interval max '600'
+set service router-advert interface eth0.110 name-server '2001:67c:1104:9::4'
+set service router-advert interface eth0.110 prefix ::/64 valid-lifetime '2592000'
